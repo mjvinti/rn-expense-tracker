091e735f3c328615dd042f8fc6a9f3cfcd1fd04c
mjvinti <mjvinti@gmail.com>
2023-10-19 16:00:18 -0400

    added conext

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -5,69 +5,6 @@ import ExpensesList from './ExpensesList';
 
 import { GlobalStyles } from '../../constants/styles';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-10-12')
-  },
-  {
-    id: 'e2',
-    description: 'A pair of pants',
-    amount: 89.29,
-    date: new Date('2022-01-15')
-  },
-  {
-    id: 'e3',
-    description: 'Bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e4',
-    description: 'Book',
-    amount: 14.99,
-    date: new Date('2022-02-19')
-  },
-  {
-    id: 'e5',
-    description: 'Another Book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-10-12')
-  },
-  {
-    id: 'e7',
-    description: 'A pair of pants',
-    amount: 89.29,
-    date: new Date('2022-01-15')
-  },
-  {
-    id: 'e8',
-    description: 'Bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e9',
-    description: 'Book',
-    amount: 14.99,
-    date: new Date('2022-02-19')
-  },
-  {
-    id: 'e10',
-    description: 'Another Book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  }
-];
-
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View style={styles.container}>
