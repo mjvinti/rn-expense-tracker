bf6c15f739522d1231b4ca82677bd22da4f3594d
mjvinti <mjvinti@gmail.com>
2023-10-20 15:40:38 -0400

    began building custom input component

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ExpenseForm = () => {
+  return (
+    <View>
+      <Text>ExpenseForm</Text>
+    </View>
+  );
+};
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({});
