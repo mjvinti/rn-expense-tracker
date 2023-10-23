5bf29856d67b5c020e12b355789031ba723b40dd
mjvinti <mjvinti@gmail.com>
2023-10-23 15:23:06 -0400

    setting the form layout

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Input from './Input';
 
@@ -6,23 +6,28 @@ const ExpenseForm = () => {
   const amountChangeHandler = () => {};
 
   return (
-    <View>
-      <Input
-        label='Amount'
-        textInputConfig={{
-          keyboardType: 'decimal-pad',
-          onChangeText: amountChangeHandler
-        }}
-      />
-      <Input
-        label='Date'
-        textInputConfig={{
-          keyboardType: 'default',
-          maxLength: 10,
-          onChangeText: () => {},
-          placeholder: 'YYYY-MM-DD'
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          label='Amount'
+          style={styles.rowInput}
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChangeText: amountChangeHandler
+          }}
+        />
+        <Input
+          label='Date'
+          style={styles.rowInput}
+          textInputConfig={{
+            keyboardType: 'default',
+            maxLength: 10,
+            onChangeText: () => {},
+            placeholder: 'YYYY-MM-DD'
+          }}
+        />
+      </View>
       <Input
         label='Description'
         textInputConfig={{
@@ -34,3 +39,19 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: { marginTop: 40 },
+  inputsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  rowInput: { flex: 1 },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginHorizontal: 24,
+    textAlign: 'center'
+  }
+});
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -2,9 +2,9 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { GlobalStyles } from '../../constants/styles';
 
-const Input = ({ label, textInputConfig }) => {
+const Input = ({ label, style, textInputConfig }) => {
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput
         style={
