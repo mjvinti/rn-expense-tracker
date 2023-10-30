8f2fa7d87ff0e628606e249a6051739147683b40
mjvinti <mjvinti@gmail.com>
2023-10-30 11:58:05 -0400

    fetching backend data

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expensesContext';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 const RecentExpenses = () => {
   const { expenses } = useContext(ExpensesContext);
+
+  useEffect(() => {
+    const getExpenses = async () => {
+      const expenses = await fetchExpenses();
+    };
+    getExpenses();
+  }, []);
 
   const recentExpenses = expenses.filter((expense) => {
     const today = new Date();
--- util/http.js
+++ util/http.js
@@ -1,5 +1,23 @@
 import axios from 'axios';
 
-export const storeExpense = (data) => {
-  axios.post(`${process.env.EXPO_PUBLIC_API_URL}/expenses.json`, data);
+const BACKEND_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export const storeExpense = (data) =>
+  axios.post(`${BACKEND_URL}/expenses.json`, data);
+
+export const fetchExpenses = async () => {
+  const response = await axios.get(`${BACKEND_URL}/expenses.json`);
+
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 };
